05a773b7724c218d451ee3f1b9e6dfb7299bf154
HanMin Chen <Shww_wowo@live.cn>
2023-02-15 15:55:11 +1100

    Added remote database for ARKit's image resources

--- THEIA/UIKit/ViewControllers/ScanTestPackagingViewController.swift
+++ THEIA/UIKit/ViewControllers/ScanTestPackagingViewController.swift
@@ -36,7 +36,12 @@ class ScanTestPackagingViewController: UIViewController, ARSessionDelegate, Data
     var testType: TestType?
     
     //The list of image paths we are recognising for
-    var imagePaths: [String?] = []
+    var imagePaths: [URL] = []
+    
+    //The number of images we have downlaoded
+    var downloadedImages: Int = 0
+    //The total number of images we need to download
+    var totalImages: Int = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -78,10 +83,39 @@ class ScanTestPackagingViewController: UIViewController, ARSessionDelegate, Data
         
     }
     
+    
+    //https://stackoverflow.com/questions/47008256/how-to-turn-on-offf-camera-flash-light-with-arkit
+    func toggleTorch(on: Bool) {
+            guard let device = AVCaptureDevice.default(for: AVMediaType.video)
+            else {return}
+
+            if device.hasTorch {
+                do {
+                    try device.lockForConfiguration()
+
+                    if on == true {
+                        device.torchMode = .on // set on
+                    } else {
+                        device.torchMode = .off // set off
+                    }
+
+                    device.unlockForConfiguration()
+                } catch {
+                    print("Torch could not be used")
+                }
+            } else {
+                print("Torch is not available")
+            }
+        }
+    
+    
     //This registers this class to receive updates from the database
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         dataBaseController?.addListener(listener: self)
+        
+        updateARResources()
+        
     }
     
     //This unregisters this class to receive updates from the database
@@ -98,8 +132,6 @@ class ScanTestPackagingViewController: UIViewController, ARSessionDelegate, Data
         let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
         //Gets the documents directory
         let documentsDirectory = paths[0].appendingPathComponent((testType?.testTypeName!)!.replacingOccurrences(of: " ", with: "-") + "-PackagingImages", conformingTo: .folder)
-        
-        print(documentsDirectory)
         
         //Check if the folder exists, if not, create one
         
@@ -134,9 +166,17 @@ class ScanTestPackagingViewController: UIViewController, ARSessionDelegate, Data
                 
                 let image = UIImage(contentsOfFile: imageURL.path)
 
+                //Add to our list of images
+                imagePaths.append(imageURL)
 
                 if let image = image {
-                    print("Image exists")
+                    //Update our downloaded total
+                    self.downloadedImages += 1
+                    //Check if this is the last image
+                    if self.downloadedImages == self.totalImages {
+                        //Update our AR Resources
+                        self.updateARResources()
+                    }
                 } else {
                     //Download it
                     let url = URL(string: imageLink!)!
@@ -158,35 +198,90 @@ class ScanTestPackagingViewController: UIViewController, ARSessionDelegate, Data
                            } catch {
                                print(error)
                            }
-                           
-                           //Try loading it
-                           let image = UIImage(contentsOfFile: imageURL.path)
-                           if let image = image {
-                               print("Got the image")
-                           } else {
-                               print("Failed to get image")
+                           //Update our downloaded total
+                           self.downloadedImages += 1
+                           if self.downloadedImages == self.totalImages{
+                               //Update our set of AR Resources
+                               self.updateARResources()
                            }
                            
                        }
                     }.resume()
-
-
                 }
 
             }
         }
     }
     
+    func updateARResources(){
+        
+        var newReferenceImages:Set<ARReferenceImage> = Set<ARReferenceImage>();
+        
+        for imagePath in imagePaths {
+            do {
+                //Load the data
+                let data = try Data(contentsOf: imagePath)
+                let image = UIImage(data: data)
+                //Get the Core Graphics Image
+                let cgImage = image!.cgImage!
+                
+                //Load the ARImage
+                let arImage = ARReferenceImage(cgImage, orientation: CGImagePropertyOrientation.up, physicalWidth: 0.09)
+                
+                //Update the name
+                arImage.name = imagePath.lastPathComponent
+                //Insert it into our set
+                newReferenceImages.insert(arImage);
+                
+                //Update our tracking
+                let configuration = ARWorldTrackingConfiguration()
+                configuration.detectionImages = newReferenceImages
+                configuration.maximumNumberOfTrackedImages = 5
+                arView.session.run(configuration, options: [.resetTracking, .removeExistingAnchors])
+                
+                
+            } catch {
+                print(error)
+            }
+            
+        }
+        
+        //Turn on flashlight
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) { [weak self] in
+            self?.toggleTorch(on: true)
+        }
+        
+        print("Updated AR Reference Images")
+        
+    }
+    
+    
     
     func session(_ session: ARSession, didAdd anchors: [ARAnchor]) {
         
-        
         for anchor in anchors{
-            
             if let imageAnchor = anchor as? ARImageAnchor {
+                //Check which anchor it is
+                guard let anchorName = imageAnchor.name else{
+                    fatalError("Failed to get name")
+                }
                 
-                
-                print("Anchor found")
+                //Find the right test
+                for test in tests{
+                    for imageLink in test.packagingImages {
+                        //Calculate the URL's hash value
+                        let inputString = imageLink
+                        let inputData = Data(inputString!.utf8)
+                        let hashed = SHA256.hash(data: inputData)
+                        let imageLinkName = hashed.description + ".png"
+                        //If they are the same
+                        if imageLinkName == anchorName {
+                            //Perform the segue
+                            self.performSegue(withIdentifier: "PackagingToTestSegue", sender: test)
+                            print("Found \(test.name)")
+                        }
+                    }
+                }
             }
         }
     }
@@ -207,6 +302,20 @@ class ScanTestPackagingViewController: UIViewController, ARSessionDelegate, Data
             let destination = segue.destination as! SearchTestsTableViewController
             
             destination.testType = testType
+        }
+        
+        if segue.identifier == "PackagingToTestSegue" {
+            
+            let destination = segue.destination as! ComponentsLearningViewController
+            
+            //Cast our sender
+            let test = sender as! Test
+            
+            destination.scannedTest = test
+            
+            //Set our torch to off
+            toggleTorch(on: false)
+            
         }
     }
     
@@ -242,6 +351,14 @@ class ScanTestPackagingViewController: UIViewController, ARSessionDelegate, Data
             }
         }
         self.tests = newTests
+        //Reset the counter
+        self.totalImages = 0
+        //Count the total number of images we need to download
+        for test in self.tests {
+            for imageLink in test.packagingImages{
+                totalImages += 1
+            }
+        }
         //Update our AR Images set
         updateARImages()
     }
